2a17561c9d818e807a58f001448d6b7424d44d19
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-07-17 16:46:04 +0300

    Fix: Mongo logs

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-// mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB Connected!'))
-//   .catch(err => console.error('MongoDb connection error:', err))
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.error('MongoDb connection error:', err))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
