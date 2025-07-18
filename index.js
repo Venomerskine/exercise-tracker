2c6c1e9c14ce172410e4073e00d3831045c3660b
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-07-18 12:10:27 +0300

    day 2 rollback

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
+require('dotenv').config()
+
+
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose')
 
-require('dotenv').config()
-
-//*MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected!'))
   .catch(err => console.error('MongoDb connection error:', err))
 
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.static('public'))
 
-//* Middleware
-
-app.use(cors());
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
