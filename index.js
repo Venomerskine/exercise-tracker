0dbe4414094cace511255e461ac93d83c33f4a15
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-07-30 17:02:15 +0300

    date formart in logs

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.get("/api/users/:_id/logs", async (req, res) => {
     const log = exercises.map(e => ({
         description: e.description,
         duration: e.duration,
-        date: e.date.toDateString()
+        date: e.date.toISOString().split('T')[0]
     }))
     
     res.json({
